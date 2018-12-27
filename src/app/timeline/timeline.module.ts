92345455aa5e4426a65ad03ce4b66efcc936fbde
yusuke™com <yusuke20023@gmail.com>
2018-12-27 09:52:16 +0900

    エラーが多すぎたので戻しました

--- src/app/timeline/timeline.module.ts
+++ src/app/timeline/timeline.module.ts
@@ -6,8 +6,6 @@ import { ChatComponent } from './chat/chat.component';
 import { TimelineRoutingModule } from './/timeline-routing.module';
 import { UserListComponent } from './user-list/user-list.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import { UserDetailComponent } from '../users/user-detail/user-detail.component';
-
 import {
   MatButtonModule,
   MatCheckboxModule,
@@ -29,14 +27,14 @@ import { UserListSearchComponent } from './user-list-search/user-list-search.com
     MatDividerModule,
     MatCardModule,
     MatFormFieldModule,
-    MatInputModule,
+    MatInputModule
   ],
   declarations: [
     ChatComponent,
     UserListComponent,
     InputComponent,
     UserListSearchComponent,
-    UserDetailComponent,
+
   ]
 })
 export class TimelineModule { }
--- src/app/timeline/user-list-search/user-list-search.component.ts
+++ src/app/timeline/user-list-search/user-list-search.component.ts
@@ -1,25 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { Observable } from 'rxjs/Observable';
-import { UserlistsearchService } from './user-list-Search.service';
-import { User } from '../../class/user';
 
 @Component({
   selector: 'app-user-list-search',
   templateUrl: './user-list-search.component.html',
-  styleUrls: ['./user-list-search.component.css'],
-  providers: [ UserlistsearchService ]
+  styleUrls: ['./user-list-search.component.css']
 })
-
 export class UserListSearchComponent implements OnInit {
-  users: Observable<User[]>;
-  UserlistsearchService: any;
- // isHit = false;
 
-  constructor(
-    private userlistsearchService: UserlistsearchService,
-    private router: Router
-  ) { }
+  constructor() { }
 
   ngOnInit() {
   }
