caf12d3703449f4eba662aadb3594a8dbb39724b
takuto <yusuke20023@gmail.com>
2020-06-06 22:57:26 +0900

    スマホ最適化

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,11 +6,13 @@ import { NotFoundComponent } from './error/not-found/not-found.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
 import { LoginComponent } from './login/login.component';
 import { AuthGuard } from './auth.guard';
+import { UserListComponent } from './timeline/user-list/user-list.component';
 
 
 const routes: Routes = [
   { path: 'timeline', loadChildren: () => import('../app/timeline/timeline.module').then(m => m.TimelineModule) },
   { path: 'users', loadChildren: () => import('../app/users/users.module').then(m => m.UsersModule) },
+  { path: 'userlist', component: UserListComponent },
   { path: 'signup', component: SignUpComponent, canActivate: [ AuthGuard ] },
   { path: 'login', component: LoginComponent, canActivate: [ AuthGuard ] },
   { path: '', redirectTo: 'timeline', pathMatch: 'full' },
